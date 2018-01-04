04b2f5a20452c01195dcf54984a938cbb125ca3d
Lance E Sloan (sloanlance) <sloanlance@users.noreply.github.com>
2018-01-04 12:57:08 -0500

    Resolve PyPI naming conflict.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # RequestsPlus
 RequestsPlus is a wrapper for the common Python "requests" library, with methods to make some tasks easier.
+
+Author: Lance E Sloan (sloanlance@gmail.com)
+
+See:
+
+* https://github.com/sloanlance/RequestsPlus
+* https://pypi.python.org/pypi/sloanlance-requestsplus
--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = 'requestsplus',
+    # Temporary name change because "requestsplus" already taken by cchen/phantomkidding!
+    # I like packages in the PHP world.  On Packagist, they're named as "author_name/package_name".
+    name = 'sloanlance_requestsplus',
     version = '0.1.0',
     keywords = ('requests', ),
     description = 'RequestsPlus is a wrapper for the common Python "requests" library, with methods to make some tasks easier.',
